19231c5662a91fc03b9d63757e3f9976b945cbfc
Misas10 <manmisa2003@gmail.com>
2021-03-02 16:00:41 +0000

    atividade2 added

---
+++ src/ficheiros/atividade2.java
@@ -0,0 +1,19 @@
+package ficheiros;
+import java.io.*;
+
+public class atividade2 {
+    public static void main(String[] args) throws IOException {
+
+        File f1 = new File("msg.txt");
+
+        FileReader fileReader = new FileReader(f1);
+        BufferedReader br = new BufferedReader(fileReader);
+
+        while(br.ready()){
+            String linha = br.readLine();
+            System.out.println(linha);
+        }
+        System.out.println();
+
+    }
+}
